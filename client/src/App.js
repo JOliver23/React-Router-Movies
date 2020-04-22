74dcd453358a9a1ff12c30d950fb258facbe4f8c
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-04-22 16:12:12 -0700

    prj ready, App wrapped in router, added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route to="/" component={MovieList}/>
+      <Route to="/movies/:id" component={Movie} />
     </div>
   );
 };
